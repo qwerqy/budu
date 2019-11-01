2d9ab2905236dc132111b620d1557262f3dc9e1e
Amin Roslan <amnrsln@gmail.com>
2019-11-02 05:00:01 +0800

    Switch back to text

--- src/components/header.js
+++ src/components/header.js
@@ -85,7 +85,7 @@ const Header = ({ siteTitle }) => (
                 }}
                 target="_blank"
               >
-                <Tukang />
+                Tukang Dev
               </a>
             </Typography>
           </Grid>
