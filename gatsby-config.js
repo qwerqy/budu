3647b45200d1f298d3607e25bcc18097695e355d
Amin Roslan <amnrsln@gmail.com>
2019-11-21 23:51:24 +0800

    Update

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,21 @@ module.exports = {
       },
     },
     `gatsby-plugin-material-ui`,
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Roboto Mono`,
+            variants: [`400`, `700`],
+          },
+          {
+            family: `Roboto`,
+            subsets: [`latin`],
+          },
+        ],
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/components/jumbo.js
+++ src/components/jumbo.js
@@ -8,11 +8,25 @@ const Jumbo = () => {
         <Box fontWeight="fontWeightBold">Hello, I'm Amin.</Box>
       </Typography>
       <Typography component="div" variant="body1">
-        <Box>
-          I am a software engineer and I specialize in writing solutions with
-          Javascript & Dart.
+        <Box mt={2}>
+          I am a software engineer, a gaming enthusiast and a tech geek. At{" "}
+          <a href="https://vase.ai">Vase.ai</a>, I am the Engineering Lead for
+          the Respondents Platform.
         </Box>
-        <Box>I am also a hardcore gamer.</Box>
+        <Box mt={2}>
+          I did not graduate from any university. Ever since I left high school,
+          I have been in various industries, working from one job to another,
+          learning and seeking for answers along the way. I got interested in
+          programming in 2018, after realising I can have the freedom to create
+          almost anything with it.
+        </Box>
+        <Box mt={2}>
+          When I'm not programming, I play games, mostly RPG genres. I am heavy
+          on the storytelling. It's kind of like therapy. I can play for days
+          when I'm immersed. I am also a binger when it comes to watching series
+          on Netflix. In between times, I love listening to music. From metal to
+          EDM to Lo-Fi.
+        </Box>
       </Typography>
     </div>
   )
